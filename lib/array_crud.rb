f8eae87d1fa6cc10885651cffa85cbe76e37f0fc
Stacy Johns <stacykjohns@icloud.com>
2019-07-11 02:52:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,8 @@ retrieve_element_from_index[2]
 end
 
 def retrieve_first_element_from_array(array)
-
+retrieve_first_element_from_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+retrieve_first_element_from_array[0]
 end
 
 def retrieve_last_element_from_array(array)
