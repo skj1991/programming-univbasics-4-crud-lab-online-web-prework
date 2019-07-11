4e178d0253ccd3ed944fbb62779db45f840bbab7
Stacy Johns <stacykjohns@icloud.com>
2019-07-11 02:24:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,6 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"]
+add_element_to_end_of_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
