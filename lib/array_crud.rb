3d50f50921aa645132a8b145ba813ef8fbd0c142
Stacy Johns <stacykjohns@icloud.com>
2019-07-11 02:34:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ add_element_to_end_of_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-
+add_element_to_start_of_array = ["I", "am", "really", "learning"]
+add_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
